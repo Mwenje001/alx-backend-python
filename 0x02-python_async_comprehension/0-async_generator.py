7aaeb1e4dca20e92f51953d6cf372c91644d42da
root <root@DESKTOP-EK7JHBM>
2023-07-11 15:18:06 +0300

    Adds O

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
-"""A coroutine called async_generator that takes no arguments"""
+""" a python module to loop 10 times """
+import random
 import asyncio
 from typing import Generator
-import random
 
 
 async def async_generator() -> Generator[float, None, None]:
-    for i in range(10):
-        await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        """
+        async_generator - function to loop 10 times
+        Arguments:
+            no arguments
+        Returns:
+            nothing
+        """
+        for i in range(10):
+            await asyncio.sleep(1)
+            yield random.uniform(0, 10)
